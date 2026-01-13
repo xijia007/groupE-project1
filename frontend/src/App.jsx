56436de31a417b7b7086dc9bff414bd5ce73d049
xijia007 <jia.xi1@northeastern.edu>
2026-01-12 21:23:47 -0800

    frontend-a global responsive Header

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import Footer from './assets/components/Footer/index.jsx'
 import Header from './assets/components/Header/index.jsx'
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages/Home.jsx'
+
 
 function App() {
   return (
-    <>
-
-        <Header />
-        <main className='mainContainer'>Project: Product Management System</main>
-        <Footer />
-    </>
+    <BrowserRouter>
+      <Header />
+      <main className='mainContainer'>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </main>
+      <Footer />
+    </BrowserRouter>
   )
 }
 
