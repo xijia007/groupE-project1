1520039be0110ae285acd4ff2e19658b8a9224b6
Zhenjia Li <zhenjia@usc.edu>
2026-01-16 09:35:20 -0800

    Some Update on frontend

--- frontend/src/assets/components/Header/index.jsx
+++ frontend/src/assets/components/Header/index.jsx
@@ -1,26 +1,31 @@
-import './Header.css'
-import { FaRegUser } from 'react-icons/fa'
-import { MdOutlineShoppingCart } from 'react-icons/md'
+import "./Header.css";
+import { FaRegUser, FaSearch } from "react-icons/fa";
+import { MdOutlineShoppingCart } from "react-icons/md";
 
-function Header() {
-    return (
-        <header className="site-header">
-            <div className="site-header-brand">Product Management System</div>
-            <div className='site-header-search'>
-                <input
-                    placeholder='Search'
-                />
-            </div>
-            <div className="site-header-userAuth">
-                 <div className='site-header-image'><FaRegUser /></div>
-                 <div className='site-header-login'>SignIn/Login</div>
-            </div>
-             <div className='site-header-shoppingcart'>
-                <MdOutlineShoppingCart />
-            </div>
-  
-        </header>
-    )
+function Header({ onSignInClick, onHomeClick }) {
+  return (
+    <header className="site-header">
+      <div className="site-header-brand" onClick={onHomeClick}>
+        <span className="brand-title">Management</span>
+        <span className="brand-title-short">M</span>
+        <span className="brand-subtitle">Chuwa</span>
+      </div>
+      <div className="site-header-search">
+        <input type="text" placeholder="Search" />
+        <FaSearch className="search-icon" />
+      </div>
+      <div className="site-header-right">
+        <div className="site-header-userAuth" onClick={onSignInClick}>
+          <FaRegUser className="site-header-image" />
+          <span className="site-header-login">Sign In</span>
+        </div>
+        <div className="site-header-cart">
+          <MdOutlineShoppingCart className="cart-icon" />
+          <span className="cart-price">$0.00</span>
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
