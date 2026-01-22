00d0806f3598e6ff2661eec621bb790f1cd4ad4e
xijia007 <jia.xi1@northeastern.edu>
2026-01-22 10:38:05 -0800

    back-to-home-page-button

--- frontend/src/pages/EditProduct.jsx
+++ frontend/src/pages/EditProduct.jsx
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+
+
 function EditProduct() {
+
   return (
     <>
       <h1>Edit Product Page</h1>
+      <Link className="back-to-home" to='/'>
+        <button>Back To Home Page</button>
+      </Link>
     </>
   );
 }
--- frontend/src/pages/ProductDetail.jsx
+++ frontend/src/pages/ProductDetail.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 function ProductDetail() {
+
     return (
         <>
             <h1>Product Detail Page</h1>
-
-
+            <Link className="back-to-home" to='/'>
+                <button>Back To Home Page</button>
+            </Link>
 
         </>
     )
