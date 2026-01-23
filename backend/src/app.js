b8475f85e0a108584a755ff68bd2615c63454bd0
Zhenjia Li <zhenjia@usc.edu>
2026-01-22 21:07:26 -0800

    Product_controller

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,23 +1,24 @@
-import express from 'express';
+import express from "express";
 import { errorHandler } from "./middlewares/errorHandler.js";
-import auth_routers from './routers.js/auth_routers.js';
-import config from './config.js';
+import auth_routers from "./routers.js/auth_routers.js";
+import product_routers from "./routers.js/product_routers.js";
+import config from "./config.js";
 
 const port = config.PORT || 3001;
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));    
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/health', (req, res) => {
-    res.json({ success: true, data: 'ok'});
+app.get("/health", (req, res) => {
+  res.json({ success: true, data: "ok" });
 });
 
-app.use('/api/auth', auth_routers);
+app.use("/api/auth", auth_routers);
+app.use("/api/products", product_routers);
 
 app.use(errorHandler);
 
-
 app.listen(port, () => {
-    console.log(`Backend running on port ${port}`);
+  console.log(`Backend running on port ${port}`);
 });
--- backend/src/routers.js/auth_routers.js
+++ backend/src/routers.js/auth_routers.js
@@ -1,10 +1,20 @@
-import {register, login, logout, UpdatePassword} from '../controllers/auth_controller.js';
-import {requireAuth} from '../middlewares/auth.js';
-import express from 'express';
+import {
+  register,
+  login,
+  logout,
+  UpdatePassword,
+  refreshToken,
+} from "../controllers/auth_controller.js";
+import { requireAuth } from "../middlewares/auth.js";
+import { me } from "../controllers/user_controller.js";
+import express from "express";
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
-router.post('/logout', requireAuth, logout);
-router.post('/update', requireAuth, UpdatePassword);
+router.post("/register", register);
+router.post("/login", login);
+router.post("/logout", requireAuth, logout);
+router.post("/update", requireAuth, UpdatePassword);
+router.get("/me", requireAuth, me);
+router.post("/refresh", refreshToken);
+
 export default router;
