98dfd666fe7a472b7317084dff6db21241264037
Zhenjia Li <zhenjia@usc.edu>
2026-01-29 20:34:39 -0800

    final update for forgotpassword

--- frontend/src/pages/auth/ForgotPassword/ForgotPasswordPage.jsx
+++ frontend/src/pages/auth/ForgotPassword/ForgotPasswordPage.jsx
@@ -1,10 +1,7 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import Auth from "../../../components/auth/Auth.jsx";
-import { useAuth } from "../../../features/auth/contexts/AuthContext.jsx";
-import { ForgotPassword } from "../../../components/auth/ForgotPassword.jsx";
-
 function ForgotPasswordPage() {
-  return <ForgotPassword />;
+  return <Auth Status="ForgotPassword" />;
 }
 
 export default ForgotPasswordPage;
